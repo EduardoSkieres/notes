903ef4684132769ed6b915ea9b207336b4f34baa
Eduardo Skieres <eduardoskieres@gmail.com>
2023-07-05 01:08:09 -0300

    Update notas

--- src/app/notes/components/note-create/note-create.component.ts
+++ src/app/notes/components/note-create/note-create.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { Note } from 'src/app/shared/models/note.model';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { NoteService } from 'src/app/shared/services/note.service';
 
 
 @Component({
@@ -7,27 +12,55 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./note-create.component.scss']
 })
 export class NoteCreateComponent implements OnInit {
-  @Output() addedNote = new EventEmitter();
-  public noteTitle: string = '';
-  public noteDescription: string = '';
+  constructor(
+    public dialogRef: MatDialogRef<NoteCreateComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Note,
+    private route: ActivatedRoute,
+    private noteService: NoteService,
 
-
-  constructor() { }
+  ) { }
 
   onSubmit() {
-    const newNote = {
-      title: this.noteTitle,
-      description: this.noteDescription,
-      isDone: false,
-      isArchived: false,
-    };
-
-    this.addedNote.emit(newNote);
-
-    this.noteTitle = '';
-    this.noteDescription = '';
+    if (this.data.id) {
+      this.save()
+    } else {
+      const newNote = {
+        id: this.newGuid(),
+        title: this.data.title,
+        description: this.data.description,
+        isDone: this.data.isDone || false,
+        isArchived: this.data.isArchived || false,
+      };
+      this.addedNote(newNote);
+    }
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
+
+
+  addedNote(newNote: Note): void {
+    this.noteService.addNote(newNote);
+    this.dialogRef.close();
+  }
+
+  delete(): void {
+    this.noteService.deleteNote(this.data.id.toString());
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    this.noteService.updateNote(this.data.id.toString(), this.data)
+    this.dialogRef.close();
+  }
+
+
+  newGuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+        v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
 }
